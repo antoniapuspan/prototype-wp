56f112c6f65e75b5fb83aa4f9e90b0ca6b4d1f40
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2020-10-26 02:00:11 +0100

    raw template

--- main.js
+++ main.js
@@ -2,6 +2,33 @@ fetch("https://antoniapuspan.com/mmd/Semester-2/t9/wordpress/wp-json/wp/v2/bike"
 .then(res => res.json())
 .then(handleData)
 
-function handleData(bike) {
-	console.log(bike);
+function handleData(bikes) {
+	console.log(bikes);
+	bikes.forEach(showBike)
+	
+}
+function showBike(bike) {
+	const template = document.querySelector("template").content;
+	console.log(bike)
+	
+	const copy = template.cloneNode(true);
+	copy.querySelector("h2").textContent=bike.title.rendered;
+	
+	let price = document.createElement("p");
+	price.innerHTML = "$" + bike.price;
+	copy.querySelector(".product_body.price").appendChild(price)
+	
+	copy.querySelector(".product.image").src = bike.image.guid;
+	
+	
+	if(bike.colours != ""){
+		let colours = document.createElement("p");
+		colours.innerHTML = bike.colours;
+		copy.querySelector(".body").appendChild(colours)
+		colourDiv.style.backgroundColor = bike.colours
+	}
+	
+	
+	
+	document.querySelector("main").appendChild(copy);
 }
